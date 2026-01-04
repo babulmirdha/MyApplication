4aa18da6ca1e0a2843fde1b5158ed47178f6dedd
Babul Mirdha <babumirdha@gmail.com>
2026-01-04 21:03:17 +0600

    sum of two numbers

--- app/src/main/java/com/alorferi/myapplication/MainActivity.kt
+++ app/src/main/java/com/alorferi/myapplication/MainActivity.kt
@@ -6,16 +6,43 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
+import com.alorferi.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+
+
+        binding.additionButton.setOnClickListener {
+
+            val firstText = binding.firstNumberEditText.text.toString().trim()
+            val secondText = binding.secondNumberEditText.text.toString().trim()
+
+            if (firstText.isEmpty() || secondText.isEmpty()) {
+                binding.resultTextView.text = "Please enter both numbers"
+                return@setOnClickListener
+            }
+
+            val firstNumber = firstText.toDouble()
+            val secondNumber = secondText.toDouble()
+
+            val result = firstNumber + secondNumber
+
+            // Remove .0 if integer
+            binding.resultTextView.text = result.toString()
+
         }
+
+
+
     }
 }
